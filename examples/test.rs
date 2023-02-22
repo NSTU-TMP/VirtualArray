6d5b2de158aef97121766794410af042e46660c6
Хромин Сергей Константинович <xromin.2021@stud.nstu.ru>
2023-02-22 11:40:49 +0700

    hard fix

--- examples/test.rs
+++ examples/test.rs
@@ -2,9 +2,9 @@ use virtual_array::VirtualArray;
 
 fn main() {
     let mut va = VirtualArray::new("test.bin", 20, 1, 20);
-    va.insert_element(0, 10);
-    va.insert_element(2, 1);
-    // dbg!(va.get_element(0));
-    // dbg!(va.get_element(2));
+    va.insert_element(0, 123);
+    va.insert_element(2, 99);
+    dbg!(va.get_element(0));
+    dbg!(va.get_element(2));
     // va.remove_element(2);
 }
